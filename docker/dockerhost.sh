9fa37115e12800db558ac411400435e1911be9d5
zw <zw@knownsec.com>
2018-07-17 19:57:52 +0800

    chore:

--- docker/dockerhost.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-grep dockerhost /etc/hosts || echo $(ip r ls | grep ^default | cut -d" " -f3) dockerhost >> /etc/hosts
--- docker/nginx.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# Include nginx defaults if available
-if [ -r /etc/default/nginx ]; then
-        . /etc/default/nginx
-fi
-DAEMON=/usr/sbin/nginx
-$DAEMON > /dev/null
